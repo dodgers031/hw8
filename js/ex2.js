e550d96ba8310f2e5f4813afc026749f0793f44b
dodgers031 <daniel.lopez14@student.csulb.edu>
2022-04-14 10:32:31 -0700

    Version 1

---
+++ js/ex2.js
@@ -0,0 +1,34 @@
+const traveler = {
+    name: "Sam",
+    countries: [
+      {
+        name: "Ecuador",
+        year: 2003
+      },
+      {
+        model: "Portugal",
+        color: 2004
+      },
+      {
+        model: "Russia",
+        color: 2016
+      }
+    ]
+  };
+  
+  
+  fetch("https://thejsway-server.herokuapp.com/api/countries", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(traveler)
+  })
+    .then(response => response.text())
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.error(err.message);
+    });
